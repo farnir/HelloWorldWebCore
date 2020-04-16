cb9ba2e6e3bb3189ec20c281d3a40dbba4ade5ad
Quentin Renaud <quentin.renaud@epitech.eu>
2020-04-16 12:19:07 +0200

    Update installApp.sh

--- installApp.sh
+++ installApp.sh
@@ -3,4 +3,4 @@ EC2_ASG="$(aws ec2 describe-tags --region="eu-west-3" | jq -r '.Tags[] | select 
 EC2_INSTANCE_ID="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 aws autoscaling complete-lifecycle-action --region="eu-west-3" --lifecycle-action-result CONTINUE \
   --instance-id $EC2_INSTANCE_ID --lifecycle-hook-name LifeCycleHookTest \
-  --auto-scaling-group-name $EC2_ASG
+  --auto-scaling-group-name $EC2_ASG || exit 0
