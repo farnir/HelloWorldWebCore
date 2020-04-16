8ced6186369eb3a085ea004823c957ef7663c896
Quentin Renaud <quentin.renaud@epitech.eu>
2020-04-16 12:38:25 +0200

    Update installApp.sh

--- installApp.sh
+++ installApp.sh
@@ -1,6 +1,6 @@
-
+yum install jq -y
 EC2_ASG="$(aws ec2 describe-tags --region="eu-west-3" | jq -r '.Tags[] | select (.Key=="aws:autoscaling:groupName") | .Value')"
 EC2_INSTANCE_ID="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 aws autoscaling complete-lifecycle-action --region="eu-west-3" --lifecycle-action-result CONTINUE \
-  --instance-id $EC2_INSTANCE_ID --lifecycle-hook-name LifeCycleHookTest \
-  --auto-scaling-group-name $EC2_ASG || exit 0
+  --instance-id "$EC2_INSTANCE_ID" --lifecycle-hook-name LifeCycleHookTest \
+  --auto-scaling-group-name "$EC2_ASG" || exit 0
