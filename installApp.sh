7dbd090874f5c12b163b0d9386293bae6bd5fa91
Quentin Renaud <quentin.renaud@epitech.eu>
2020-04-16 11:53:51 +0200

    Update installApp.sh

--- installApp.sh
+++ installApp.sh
@@ -1,6 +1,4 @@
 
-sudo killall -9 dotnet
-nohup dotnet /home/ec2-user/src/HelloWorldWebCore/build_output/HelloWorldWebCore.dll &>/dev/null &
 EC2_ASG="$(aws ec2 describe-tags --region="eu-west-3" | jq -r '.Tags[] | select (.Key=="aws:autoscaling:groupName") | .Value')"
 EC2_INSTANCE_ID="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 aws autoscaling complete-lifecycle-action --region="eu-west-3" --lifecycle-action-result CONTINUE \
